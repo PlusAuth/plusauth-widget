4b304bf5fe11b8c8a71283865c5c6ecb630d1fef
ayazismailhakki <ayazismailhakki@gmail.com>
2020-04-29 07:29:11 +0300

    refactor: reorganize router

--- src/ui/utils/router.ts
+++ src/ui/utils/router.ts
@@ -1,43 +1,78 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { defineComponent, h } from 'vue';
+import { createRouter, createWebHistory, View } from 'vue-router';
 
 import Consent from '../views/Consent.vue';
 import FillMissing from '../views/FillMissing.vue';
 import ForgotPassword from '../views/ForgotPassword.vue';
 import Login from '../views/Login.vue';
+import Challenge from '../views/mfa/Challenge.vue';
+import Email from '../views/mfa/Email.vue';
+import GA from '../views/mfa/GA.vue';
+import SMS from '../views/mfa/SMS.vue';
 import Register from '../views/Register.vue';
 import ResetPassword from '../views/ResetPassword.vue';
 
+const PlainRouterView = defineComponent( {
+  render(){
+    return h(View)
+  }
+})
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/signin',
-      // @ts-ignore
-      component: Login,
+      component: PlainRouterView,
+      children: [
+        {
+          path: '',
+          component: Login
+        },
+        {
+          path: '/consent',
+          component: Consent
+        },
+        {
+          path: '/recovery',
+          component: ForgotPassword
+        },
+        {
+          path: 'challenge',
+          component: PlainRouterView,
+          children: [
+            {
+              path: '',
+              component: Challenge
+            },
+            {
+              path: 'sms',
+              name: 'sms',
+              component: SMS,
+            },
+            {
+              path: '/email',
+              name: 'email',
+              component: Email
+            },
+            {
+              path: '/ga',
+              name: 'ga',
+              component: GA
+            }
+          ]
+        },
+      ]
     },
     {
       path: '/signup',
-      // @ts-ignore
       component: Register
     },
     {
-      path: '/signin/recovery',
-      // @ts-ignore
-      component: ForgotPassword
-    },
-    {
       path: '/updateMissingInformation',
-      // @ts-ignore
       component: FillMissing,
     },
     {
-      path: '/signin/consent',
-      // @ts-ignore
-      component: Consent
-    },
-    {
       path: '/resetPassword/:token?',
-      // @ts-ignore
       component: ResetPassword
     }
   ]
