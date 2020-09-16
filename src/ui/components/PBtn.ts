9c7dfbfe1b78d32489b27783135ee2ab2a551326
ayazismailhakki <ayazismailhakki@gmail.com>
2020-09-16 20:44:42 +0300

    feat: add flat prop and textColor to button component

--- src/ui/components/PBtn.ts
+++ src/ui/components/PBtn.ts
@@ -12,37 +12,48 @@ function getContent(this: ComponentPublicInstance<any>){
 }
 export default defineComponent({
   name: 'PBtn',
-  mixins: [Colorable],
+  mixins: [Colorable, Themeable],
   props: {
     ...Colorable.props,
     loading: { type: Boolean },
-    block: { type: Boolean, default: false }
+    block: { type: Boolean, default: false },
+    flat: { type: Boolean, default: false }
   },
   methods: {
     genLoader(){
       return h('span', {
         class: 'pa__btn__loader',
-      }, (this.$slots.loader ? this.$slots.loader() : false) || [h(Loading, {
-        indeterminate: true,
-        size: 23,
-        width: 2,
-      })])
+      },
+      (this.$slots.loader ? this.$slots.loader() : false) || [
+        h(Loading, {
+          indeterminate: true,
+          size: 23,
+          width: 2,
+        })
+      ])
     }
   },
   render(){
-    return h('button', this.setBackgroundColor(this.color, {
-      ...this.$attrs,
-      class: {
-        'pa__btn': true,
-        'pa__size--default': true,
-        'pa__btn--loading': !!this.loading,
-        'pa__btn--block': this.block
-      },
-    }),
-    [
-      this.loading ? this.genLoader() : h('div', {
-        class: ['pa__btn__content', 'pa__size--default']
-      },  [getContent.call(this)])
-    ])
+    return h('button',
+      this.setBackgroundColor(this.color, {
+        ...this.$attrs,
+        class: {
+          'pa__btn': true,
+          'pa__size--default': true,
+          'pa__btn--loading': !!this.loading,
+          'pa__btn--block': this.block,
+          'pa__btn--flat': this.flat
+        },
+      }),
+      [
+        this.loading ? this.genLoader() :
+
+          h('div',
+            this.setTextColor(this.textColor,{
+              class: ['pa__btn__content', 'pa__size--default']
+            }),
+            [getContent.call(this)]
+          )
+      ])
   }
 })
