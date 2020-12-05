5b90eaab6c49cafee2dbe65eefdffb831e609868
ayazismailhakki <ayazismailhakki@gmail.com>
2020-12-06 00:13:25 +0300

    perf: include popper to vite's optimized dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import {UserConfig} from "vite";
 const config: UserConfig = {
   plugins: [
   ],
+  optimizeDeps: {
+    include: [
+      "@popperjs/core/lib/modifiers/offset",
+      "@popperjs/core/lib/modifiers/flip",
+      "@popperjs/core/lib/modifiers/preventOverflow",
+      "@popperjs/core/lib/popper-lite"
+    ]
+  }
 }
 export default config
