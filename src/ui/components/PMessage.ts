d7a1c05e41b0c2ad1a3f05d8a81af5ffdf575b89
Ismail H. Ayaz <ayazismailhakki@gmail.com>
2021-02-15 14:20:32 +0300

    feat: allow translate object to be passed to pmessage

--- src/ui/components/PMessage.ts
+++ src/ui/components/PMessage.ts
@@ -3,7 +3,22 @@ import { defineComponent, h, withDirectives, VNode, Transition } from 'vue';
 import { i18n } from '../directives/i18n';
 import { Colorable } from '../mixins';
 
-function generateMessageVNode(message: string, name: string): VNode{
+function generateMessageVNode(message: string | Record<string, any>, name: string): VNode {
+  if (message && typeof message === 'object') {
+    return withDirectives(
+      h('div', {
+        class: 'pa__messages__message',
+      }),
+      [[i18n, {
+        args: {
+          ...message.args,
+          field: name && `common.fields.${name}`
+        },
+        path: message.path
+      }
+      ]]
+    )
+  }
   return withDirectives(
     h('div', {
       class: 'pa__messages__message',
@@ -13,12 +28,12 @@ function generateMessageVNode(message: string, name: string): VNode{
 }
 
 function generateMessages(messages: any, name: string): VNode | VNode[] | undefined {
-  if(!messages){
+  if (!messages) {
     return undefined
   }
-  if(Array.isArray(messages)){
+  if (Array.isArray(messages)) {
     return messages.map(message => generateMessageVNode(message, name))
-  }else if( messages instanceof Set) {
+  } else if (messages instanceof Set) {
     const nodes = []
     for (const value of messages.values()) {
       nodes.push(
@@ -26,10 +41,11 @@ function generateMessages(messages: any, name: string): VNode | VNode[] | undefi
       )
     }
     return nodes
-  }else{
+  } else {
     return generateMessageVNode(messages, name)
   }
 }
+
 export default defineComponent({
   name: 'PMessage',
   mixins: [Colorable],
@@ -38,15 +54,15 @@ export default defineComponent({
     value: { type: [String, Array], default: null },
     field: { type: String, default: null }
   },
-  render(){
-    return  h(
+  render() {
+    return h(
       'div',
       this.setTextColor(this.color, {
         class: {
           'pa__messages': true
         }
       }),
-      h(Transition,{ name: 'message-transition', css: true },
+      h(Transition, { name: 'message-transition', css: true },
         { default: generateMessages.bind(this, this.value, this.field) }
       )
     )
