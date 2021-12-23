be0b46633f6278c421d5109695d9a3c88c42731f
Ismail H. Ayaz <ayazismailhakki@gmail.com>
2021-12-23 21:30:26 +0300

    chore: use release-it hooks

--- .release-it.js
+++ .release-it.js
@@ -1,4 +1,8 @@
 module.exports = {
+  hooks:{
+    "before:init": ["npm run lint", "npm test"],
+    "after:bump": ["npm run build"],
+  },
   "git": {
     "commitMessage": "chore: release v${version}",
     "requireCleanWorkingDir": true,
