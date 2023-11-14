307e2f60a4c873499b24815005c47fd7d308752e
Ismail H. Ayaz <ayazismailhakki@gmail.com>
2023-11-14 11:51:46 +0300

    feat: provide fetch wrapper as `http` to widget

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,20 @@ import { App, reactive } from 'vue';
 import defaultDictionary from './i18n'
 import { createTranslator, createWidget } from './ui';
 import { IPlusAuthContext, IWidgetSettings } from './ui/interfaces';
+import { createFetchWrapper, FetchWrapper } from './ui/utils/fetch';
 import { Translator } from './ui/utils/translator';
 
 export default class PlusAuthWidget {
   private _view: App<Element>;
   public api: PlusAuthWeb;
   public i18n: Translator;
+  public http: FetchWrapper;
 
   constructor(container: Element | string,
     settings: Partial<IWidgetSettings> = {},
     context: Partial<IPlusAuthContext>) {
 
+    this.http = createFetchWrapper()
     this.api = new PlusAuthWeb(settings.apiUrl ||
     location.origin !== 'null' ? window.location.origin : '/')
 
@@ -29,6 +32,7 @@ export default class PlusAuthWidget {
     this.i18n = createTranslator(reactiveSettings.locale)
     this._view = createWidget(container || document.body, reactiveSettings, context, this.i18n)
     this._view.provide('api', this.api)
+    this._view.provide('http', this.http)
   }
   get view(): IWidgetSettings {
     // expose settings rather than vue app
