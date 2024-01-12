cf0dc649c8f3f1dda59c894faebbcf891b962e55
Ismail H. Ayaz <ayazismailhakki@gmail.com>
2024-01-12 17:55:16 +0300

    fix: sub routes not resolved

--- src/ui/utils/router.ts
+++ src/ui/utils/router.ts
@@ -20,71 +20,74 @@ import ResetPassword from '../views/ResetPassword.vue';
 import VerifyEmail from '../views/VerifyEmail.vue';
 
 export function resolveView(mode?: string): DefineComponent<any, any, any, any, any> {
-  mode = mode ? mode.toLowerCase().replaceAll('-', '').replaceAll('_', '') : ''
+  const m = mode ? mode.toLowerCase().replaceAll('-', '').replaceAll('_', '') : ''
   const parts = window.location.pathname.split('/').slice(1)
 
-  if (mode === 'consent' || parts[0] === 'signin' && parts[1] === 'consent') {
+  if (m === 'consent' || parts[0] === 'signin' && parts[1] === 'consent') {
     return Consent
   }
-  if (mode === 'passwordrecovery' || parts[0] === 'signin' && parts[1] === 'recovery') {
+  if (m === 'passwordrecovery' || parts[0] === 'signin' && parts[1] === 'recovery') {
     return ForgotPassword
   }
 
-  if (mode === 'login' || parts[0] === 'signin') {
+  if (parts[0] === 'signin' || m.startsWith('passwordless')) {
+    // PASSWORDLESS
+
+    if (m === 'passwordlessemail' || parts[1] === 'passwordless' && parts[2] === 'email') {
+      return PasswordlessEmail
+    }
+    if (m === 'passwordlesssms' || parts[1] === 'passwordless' && parts[2] === 'sms') {
+      return PasswordlessSMS
+    }
+    if (m === 'passwordlessotp' || parts[1] === 'passwordless' && parts[2] === 'otp') {
+      return PasswordlessOTP
+    }
+    if (m === 'passwordlesspush' || parts[1] === 'passwordless' && parts[2] === 'push') {
+      return PasswordlessPush
+    }
+  }
+
+  if (parts[0] === 'signin' || m.startsWith('mfa')) {
+
+    // MFA CHALLENGES
+    if (m === 'mfaemail' || parts[1] === 'challenge' && parts[2] === 'email') {
+      return MFAEmail
+    }
+    if (m === 'mfafv' || parts[1] === 'challenge' && parts[2] === 'fv') {
+      return MFAFingerVein
+    }
+    if (m === 'mfasms' || parts[1] === 'challenge' && parts[2] === 'sms') {
+      return MFASMS
+    }
+    if (m === 'mfaotp' || parts[1] === 'challenge' && parts[2] === 'otp') {
+      return MFAOTP
+    }
+    if (m === 'mfapush' || parts[1] === 'challenge' && parts[2] === 'push') {
+      return MFAPush
+    }
+    if (m === 'mfawebauthn' || parts[1] === 'challenge' && parts[2] === 'webauthn') {
+      return MFAWebauthN
+    }
+    if (m === 'mfa' || parts[1] === 'challenge') {
+      return MFAChallenge
+    }
+  }
+
+  if (m === 'login' || parts[0] === 'signin' ) {
     return Login
   }
 
-  if (mode === 'register' || parts[0] === 'signup') {
+  if (m === 'register' || parts[0] === 'signup' && !parts[1]) {
     return Register
   }
 
-
-  // PASSWORDLESS
-
-  if (mode === 'passwordlessemail' || parts[1] === 'passwordless' && parts[2] === 'email') {
-    return PasswordlessEmail
-  }
-  if (mode === 'passwordlesssms' || parts[1] === 'passwordless' && parts[2] === 'sms') {
-    return PasswordlessSMS
-  }
-  if (mode === 'passwordlessotp' || parts[1] === 'passwordless' && parts[2] === 'otp') {
-    return PasswordlessOTP
-  }
-  if (mode === 'passwordlesspush' || parts[1] === 'passwordless' && parts[2] === 'push') {
-    return PasswordlessPush
-  }
-
-  // MFA CHALLENGES
-  if (mode === 'mfaemail' || parts[1] === 'challenge' && parts[2] === 'email') {
-    return MFAEmail
-  }
-  if (mode === 'mfafv' || parts[1] === 'challenge' && parts[2] === 'fv') {
-    return MFAFingerVein
-  }
-  if (mode === 'mfasms' || parts[1] === 'challenge' && parts[2] === 'sms') {
-    return MFASMS
-  }
-  if (mode === 'mfaotp' || parts[1] === 'challenge' && parts[2] === 'otp') {
-    return MFAOTP
-  }
-  if (mode === 'mfapush' || parts[1] === 'challenge' && parts[2] === 'push') {
-    return MFAPush
-  }
-  if (mode === 'mfawebauthn' || parts[1] === 'challenge' && parts[2] === 'webauthn') {
-    return MFAWebauthN
-  }
-  if (mode === 'mfa' || parts[1] === 'challenge') {
-    return MFAChallenge
-  }
-
-
-  if (mode === 'verifyemail' || parts[0] === 'account' && parts[1] === 'verifyEmail') {
+  if (m === 'verifyemail' || parts[0] === 'account' && parts[1] === 'verifyEmail') {
     return VerifyEmail
   }
-  if (mode === 'fillmissing' || parts[0] === 'account' && parts[1] === 'updateMissingInformation') {
+  if (m === 'fillmissing' || parts[0] === 'account' && parts[1] === 'updateMissingInformation') {
     return FillMissing
   }
-  if (mode === 'resetpassword' || parts[0] === 'account' && parts[1] === 'resetPassword') {
+  if (m === 'resetpassword' || parts[0] === 'account' && parts[1] === 'resetPassword') {
     return ResetPassword
   }
 }
