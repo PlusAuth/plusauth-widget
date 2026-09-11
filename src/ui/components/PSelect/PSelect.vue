437076b665c4d99e98f57ddbcbda9bf3c49d2a71
Emir Yaman Gazaz <emiryaman.gazaz@ekinokssoftware.com>
2026-09-11 21:20:11 +0300

    style: better selectbox

--- src/ui/components/PSelect/PSelect.vue
+++ src/ui/components/PSelect/PSelect.vue
@@ -272,20 +272,6 @@ const classes = computed(() => ({
           @keydown.enter="onItemClick($event, item)"
         >
           <span class="pa__input-select-item-text">{{ getItemText(item) }}</span>
-          <svg
-            v-if="getItemValue(item) === internalValue"
-            class="pa__input-select-item-check"
-            width="14"
-            height="14"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <polyline points="20 6 9 17 4 12" />
-          </svg>
         </div>
       </div>
     </div>
